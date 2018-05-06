4b9156b2b07e2d2e8465cd7f8550b21c2c064064
ktrojanek89 <ktrojanek89@users.noreply.github.com>
2018-05-06 12:22:06 +0200

    zmiana 3 0605

--- ssh_cwiczenie.py
+++ ssh_cwiczenie.py
@@ -1,4 +1,6 @@
 #!USR/BIN/PYTHON
+
+#niezmienna czesc 1
 import paramiko
 #print "Ala"
 zmienna=paramiko.SSHClient()
@@ -7,8 +9,34 @@ zmienna.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 zmienna.connect("localhost", username='tester02', password='karolina')
 wejscie,wyjscie,bledy=zmienna.exec_command("cat /etc/passwd")
 rezultat=wyjscie.read()
+
+#czesc 2
 lista_linii=rezultat.split("\n")
+tester03_jest=False
+
 for el in lista_linii:
     if el.find("tester02")+1:
-        print(el)
+        tester03_jest=True
+        #print(el)
         break
+
+if tester03_jest:
+    print("istnieje")
+else:
+    print("nie istnieje")
+
+#czesc 3
+
+lista_linii=rezultat.split("\n")
+innyuzytkownik=False
+
+for el in lista_linii:
+    if el.find("tester02")+1:
+        #innyuzytkownik=True
+        #print(el)
+        break
+
+if innyuzytkownik:
+    print("istnieje")
+else:
+    print("nie istnieje")
