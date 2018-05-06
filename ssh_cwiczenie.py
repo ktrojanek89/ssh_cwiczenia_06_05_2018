7022a6f88df392102628a06ea82ded7891392884
ktrojanek89 <ktrojanek89@users.noreply.github.com>
2018-05-06 10:36:08 +0200

    ssh1 06.05

---
+++ ssh_cwiczenie.py
@@ -0,0 +1,9 @@
+#!USR/BIN/PYTHON
+import paramiko
+print "Ala"
+zmienna=paramiko.SSHClient()
+type(zmienna)
+zmienna.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+zmienna.connect("localhost", username='tester02', password='karolina')
+wejscie,wyjscie,bledy=zmienna.exec_command("uname -a")
+print wyjscie.read()
